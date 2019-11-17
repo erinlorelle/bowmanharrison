f354100f928301be88e95b34923dd1642868973e
Erin <zelc10@etsu.edu>
2019-11-17 17:23:19 -0500

    test7

--- resources/views/buy.blade.php
+++ resources/views/buy.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <section class="testimonials text-center bg-light">
         <div class="container">
-            <h2 class="mb-5">Buy</h2>
+            <h2 class="mb-5">Buy a house</h2>
         </div>
     </section>
 @endsection
