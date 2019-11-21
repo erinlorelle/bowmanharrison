62d34f0d7196ea173a4d2c8e9adac09a0a1159db
Erin <zelc10@etsu.edu>
2019-11-21 18:43:43 -0500

    helped Elliott modify staging div

--- resources/views/staging.blade.php
+++ resources/views/staging.blade.php
@@ -2,14 +2,8 @@
 
 @section('content')
 
-
-
     <section class="testimonials text-center bg-light">
-
-
-
         <div class="container">
-
             <h2>Staging Services</h2><br>
             <a href="guides#stage"><h4>Home Staging Guide</h4></a>
             <a href="faq"><h4>FAQs</h4></a>
@@ -18,34 +12,22 @@
             <br>
             <h3>Portfolio</h3>
 
-
-
-            <div class="gallery">
-                <a target="_blank" href="img/after1.jpg">
-                    <img src="img/before1.jpg" alt="before1" width="600" height="400">
-                </a>
-
-            </div>
-
-            <div class="gallery">
-                <a target="_blank" href="img/after2.jpg">
-                    <img src="img/before2.jpg" alt="Forest" width="600" height="400">
-                </a>
-
-            </div>
-
-            <div class="gallery">
-                <a target="_blank" href="img/after2.jpg">
-                    <img src="img/before2.jpg" alt="Forest" width="600" height="400">
-                </a>
-
-            </div>
-
-            <div class="gallery">
-                <a target="_blank" href="img_mountains.jpg">
-                    <img src="img_mountains.jpg" alt="Mountains" width="600" height="400">
-                </a>
-
+            <div class="row">
+                <div class="column">
+                    <a target="_blank" href="img/after1.jpg">
+                        <img class="gallery" src="img/before1.jpg" alt="before1" style="width:100%">
+                    </a>
+                </div>
+                <div class="column">
+                    <a target="_blank" href="img/after2.jpg">
+                        <img class="gallery" src="img/before2.jpg" alt="Forest" style="width:100%">
+                    </a>
+                </div>
+                <div class="column">
+                    <a target="_blank" href="img/after2.jpg">
+                        <img class="gallery" src="img/before2.jpg" alt="Forest" style="width:100%">
+                    </a>
+                </div>
             </div>
 
         </div>
