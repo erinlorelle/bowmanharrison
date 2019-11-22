161d3511085711e6e16856d4bf8350837a8f3a7a
Elliott <poffje@etsu.edu>
2019-11-21 20:26:24 -0500

    Merge branch 'development' into elliott-development
    # Conflicts:
    #	.idea/workspace.xml

--- resources/views/aboutjamietami.blade.php
+++ resources/views/aboutjamietami.blade.php
@@ -2,13 +2,33 @@
 
 @section('content')
 
+    {{--<section class="testimonials text-center bg-light">
+        <div class="container">
+            <h2 class="mb-5">About Jamie and Tami</h2>
+            <p class="lead mb-0">Learn more about Jamie and Tami, they're history, experience, and professional qualifications!</p>
+            <figure style="float:right">
+                <img src="img/p2.jpg" width="400" height="475" alt="Jamie Bowman and Tami Harrison">
+                <figcaption><em>Jamie Bowman and Tami Harrison</em></figcaption>
+            </figure>
+
+
+            <h3>History</h3>
+
+                <img src="img/allthree.jpg" width="500" height="475" alt="Jamie Bowman and Tami Harrison" style="float:left">
+
+
+            <h3>Professional Qualifications</h3>
+            <h3>Experience</h3>
+        </div>
+    </section>--}}
+
 
     <header class="jamiehead text-white text-center">
         <div class="overlay"></div>
         <div class="container">
             <div class="row">
                 <div class="col-xl-9 mx-auto">
-                    <h2>About Jamie and Tami</h2>
+                    <h1>About Jamie and Tami</h1>
                 </div>
                 <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
 
@@ -17,57 +37,66 @@
         </div>
     </header>
 
-    <article class="mt-5 mb-2">
+    <section class="testimonials text-center bg-light">
         <div class="container">
 
-
-
             <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
-                <div class="resume-content">
-                    <section class="history">
-                        <h3 class="mb-0" style="text-align: left">History</h3>
+                <div class="resume-content" style="text-align: left;">
+                    <h3 class="mb-0" style="text-align: left">History</h3>
+                    {{--<div class="subheading mb-3">Intelitec Solutions</div>--}}
+                    <p>Tami and Jamie are a mother/daughter team with professional experience in real estate buying, selling and staging.</p>
 
-                        <p class="mt-3 pr-3">Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.</p>
-                    </section>
+                    <p>We have helped many families achieve their home ownership dreams through strategically navigating buyers and sellers through the entire process.</p>
 
-                    <section class="experience pt-3">
-                        <h3 class="mb-0">Experience</h3>
-
-                        <p class="mt-3 pr-3">Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</p>
-
-                    </section>
-
-                    <section class="qualification pt-3">
-                        <h3 class="mb-0">Qualifications</h3>
-
-                        <p class="mt-3 pr-3">Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.</p>
-
-                    </section>
-
-
+                    <p>Your time is precious and you need a team that you can easily communicate with and has real life experience in all facets of the real estate market.</p>
                 </div>
-                <div class="resume-date mt-2" style="text-align: center">
-
+                <div class="resume-date text-md-right">
+                   {{-- <span class="text-primary">March 2013 - Present</span>--}}
                     <figure>
-                        <img src="img/jamietami.jpg" width=350" alt="Jamie Bowman and Tami Harrison">
-                        <figcaption class="text-center"><em>Jamie Bowman and Tami Harrison</em></figcaption>
+                        <img src="img/jamietami.jpg" width="200" height="175" alt="Jamie Bowman and Tami Harrison">
+                        <figcaption style="text-align: center"><em>Jamie Bowman and Tami Harrison</em></figcaption>
                     </figure>
                 </div>
             </div>
-        </div>
-    </article>
-    <section class="pt-5 pb-5 bg-light">
 
-        <div class="container text-center">
-            <h3 class="mb-4">Testimonials, Awards, Accolades</h3>
-            <a href="testimonials"><img class="img-fluid rounded-circle mb-3" width="300"  src="img/awards.png" alt=""></a>
+            <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="resume-content">
+                    <figure>
+                        <img src="img/allthree.jpg" width="200" height="155" alt="Jamie Bowman and Tami Harrison">
+                        <figcaption><em>Jamie Bowman and Tami Harrison Family</em></figcaption>
+                    </figure>
+                    {{--<h3 class="mb-0">Experience</h3>--}}
+                   {{-- <div class="subheading mb-3">Intelitec Solutions</div>--}}
+                    {{--<p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</p>--}}
+                </div>
+                <div class="resume-date text-md-right">
+                    <h3 class="mb-0">Experience</h3>
+                    {{-- <div class="subheading mb-3">Intelitec Solutions</div>--}}
+                    <p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</p>
+
+                </div>
+            </div>
+
+            <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="resume-content">
+                    <h3 class="mb-0" style="text-align: left">Qualifications</h3>
+                    {{--<div class="subheading mb-3">Intelitec Solutions</div>--}}
+                    <p style="text-align: left;">We are both members of the Northeast Tennessee Association of Realtors and are affiliate brokers working for Crye-Leike Realtors. We also hold certification of relocation specialists through LeadingRE.</p>
+                </div>
+                <div class="resume-date text-md-right">
+                    {{-- <span class="text-primary">March 2013 - Present</span>--}}
+                    <figure>
+                        <img src="img/jamie-tami.jpg" width="200" height="145" alt="Jamie Bowman and Tami Harrison">
+                        <figcaption style="text-align: center"><em>Jamie Bowman and Tami Harrison</em></figcaption>
+                    </figure>
+                </div>
+            </div>
 
         </div>
+        <h3>Testimonials, Awards, Accolades</h3>
+        <a href="testimonials"><img class="img-fluid rounded-circle mb-3" width="200" height="200" src="img/awards.png" alt=""></a>
 
     </section>
-
-
-
 
 
 @endsection
--- resources/views/staging.blade.php
+++ resources/views/staging.blade.php
@@ -2,17 +2,69 @@
 
 @section('content')
 
+
+
     <section class="testimonials text-center bg-light">
+
+        <script type="text/javascript">
+
+            function setImageVisible(id, visible) {
+                var img = document.getElementById(id);
+                img.style.visibility = (visible ? 'visible' : 'hidden');
+            }
+        </script>
+
         <div class="container">
-            <h2>Staging Services</h2><br>
+
+            <h1>Staging Services</h1><br>
             <a href="guides#stage"><h4>Home Staging Guide</h4></a>
             <a href="faq"><h4>FAQs</h4></a>
             <br>
+
+            <h3>Click the images below to see how we can increase your home's value!</h3>
             <br>
-            <br>
-            <h3>Portfolio</h3>
+            <div class="row">
+            <div class="gallery">
+                <div class="parent">
+                    <a onclick="setImageVisible('img1', true)">
+                    <img class="image1" src="img/before1.jpg" alt="before1" width="600" height="400">
+                    <img class="image2" id="img1" src="img/after1.jpg" style="visibility:hidden"/>
+                    </a>
+                </div>
+
+            </div>
+            <div class="gallery">
+                <div class="parent">
+                    <a onclick="setImageVisible('img2', true)">
+                        <img class="image1" src="img/before2.jpg" alt="before1" width="600" height="400">
+                        <img class="image2" id="img2" src="img/after2.jpg" style="visibility:hidden"/>
+                    </a>
+                </div>
+            </div>
+            </div>
+
+            <div class="row">
+                <div class="gallery">
+                    <div class="parent">
+                        <a onclick="setImageVisible('img3', true)">
+                            <img class="image1" src="img/before3.jpg" alt="before1" width="600" height="400">
+                            <img class="image2" id="img3" src="img/after3.jpg" style="visibility:hidden"/>
+                        </a>
+                    </div>
+                </div>
+                <div class="gallery">
+                    <div class="parent">
+                        <a onclick="setImageVisible('img4', true)">
+                            <img class="image1" src="img/before4.jpg" alt="before1" width="600" height="400">
+                            <img class="image2" id="img4" src="img/after4.jpg" style="visibility:hidden"/>
+                        </a>
+                    </div>
+
+                </div>
+            </div>
 
         </div>
+
     </section>
 
 
