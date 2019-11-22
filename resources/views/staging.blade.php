ce33c846c4cd0a1f712a693ad1670d7a3473b346
Elliott <poffje@etsu.edu>
2019-11-21 19:59:36 -0500

    Created a clickable before and after gallery for the staging images. Also added more of the staging images to the project.

--- resources/views/staging.blade.php
+++ resources/views/staging.blade.php
@@ -6,49 +6,65 @@
 
     <section class="testimonials text-center bg-light">
 
+        <script type="text/javascript">
 
+            function setImageVisible(id, visible) {
+                var img = document.getElementById(id);
+                img.style.visibility = (visible ? 'visible' : 'hidden');
+            }
+        </script>
 
         <div class="container">
 
-            <h2>Staging Services</h2><br>
+            <h1>Staging Services</h1><br>
             <a href="guides#stage"><h4>Home Staging Guide</h4></a>
             <a href="faq"><h4>FAQs</h4></a>
             <br>
+
+            <h3>Click the images below to see how we can increase your home's value!</h3>
             <br>
-            <br>
-            <h3>Portfolio</h3>
-
-
-
+            <div class="row">
             <div class="gallery">
-                <a target="_blank" href="img/after1.jpg">
-                    <img src="img/before1.jpg" alt="before1" width="600" height="400">
-                </a>
+                <div class="parent">
+                    <a onclick="setImageVisible('img1', true)">
+                    <img class="image1" src="img/before1.jpg" alt="before1" width="600" height="400">
+                    <img class="image2" id="img1" src="img/after1.jpg" style="visibility:hidden"/>
+                    </a>
+                </div>
 
             </div>
-
             <div class="gallery">
-                <a target="_blank" href="img/after2.jpg">
-                    <img src="img/before2.jpg" alt="Forest" width="600" height="400">
-                </a>
-
+                <div class="parent">
+                    <a onclick="setImageVisible('img2', true)">
+                        <img class="image1" src="img/before2.jpg" alt="before1" width="600" height="400">
+                        <img class="image2" id="img2" src="img/after2.jpg" style="visibility:hidden"/>
+                    </a>
+                </div>
+            </div>
             </div>
 
-            <div class="gallery">
-                <a target="_blank" href="img/after2.jpg">
-                    <img src="img/before2.jpg" alt="Forest" width="600" height="400">
-                </a>
+            <div class="row">
+                <div class="gallery">
+                    <div class="parent">
+                        <a onclick="setImageVisible('img3', true)">
+                            <img class="image1" src="img/before3.jpg" alt="before1" width="600" height="400">
+                            <img class="image2" id="img3" src="img/after3.jpg" style="visibility:hidden"/>
+                        </a>
+                    </div>
+                </div>
+                <div class="gallery">
+                    <div class="parent">
+                        <a onclick="setImageVisible('img4', true)">
+                            <img class="image1" src="img/before4.jpg" alt="before1" width="600" height="400">
+                            <img class="image2" id="img4" src="img/after4.jpg" style="visibility:hidden"/>
+                        </a>
+                    </div>
 
-            </div>
-
-            <div class="gallery">
-                <a target="_blank" href="img_mountains.jpg">
-                    <img src="img_mountains.jpg" alt="Mountains" width="600" height="400">
-                </a>
-
+                </div>
             </div>
 
         </div>
+
     </section>
 
 
