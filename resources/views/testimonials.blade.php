8fa92c4ffdb580dc5e67e24cfa6efde70e80dc42
Erin <zelc10@etsu.edu>
2019-11-26 18:49:11 -0500

    testimonials font color

--- resources/views/testimonials.blade.php
+++ resources/views/testimonials.blade.php
@@ -33,71 +33,71 @@
     </section>
     <hr>
     <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="education">
-        <div class="w-100">
-            <h2 class="mb-5" style="text-align: center;">Awards and Accolades</h2>
+        <div class="container">
+            <div class="w-100">
+                <h2 class="mb-5" style="text-align: center;">Awards and Accolades</h2>
 
-            <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
-                <div class="resume-content">
-                    <h4>Northeast Tennessee Association of Realtors</h4>
-                    <div class="subheading mb-3">Both Jamie Bowman and Tami Harrison are members </div>
-                    {{--<div>Computer Science - Web Development Track</div>
-                    <p>GPA: 3.23</p>--}}
+                <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="resume-content">
+                        <h4>Northeast Tennessee Association of Realtors</h4>
+                        <div class="subheading mb-3">Both Jamie Bowman and Tami Harrison are members </div>
+                        {{--<div>Computer Science - Web Development Track</div>
+                        <p>GPA: 3.23</p>--}}
+                    </div>
+                    <div class="resume-date text-md-right">
+                        <p>Awarded: July 2019</p>
+                    </div>
                 </div>
-                <div class="resume-date text-md-right">
-                    <span class="text-primary" style="color:black;">Awarded: October 2018</span>
+
+                <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="resume-content">
+                        <h4>National Realtor Certificate</h4>
+                        <div class="subheading mb-3">Certificate signifying nationwide acceptance of their accomplishments</div>
+                        {{--<div>Computer Science - Web Development Track</div>
+                        <p>GPA: 3.23</p>--}}
+                    </div>
+                    <div class="resume-date text-md-right">
+                        <p>Awarded: July 2019</p>
+                    </div>
                 </div>
+
+                <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="resume-content">
+                        <h4>National Realtor Certificate</h4>
+                        <div class="subheading mb-3">Certificate signifying nationwide acceptance of their accomplishments</div>
+                        {{--<div>Computer Science - Web Development Track</div>
+                        <p>GPA: 3.23</p>--}}
+                    </div>
+                    <div class="resume-date text-md-right">
+                        <p>Awarded: July 2019</p>
+                    </div>
+                </div>
+
+                <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="resume-content">
+                        <h4>Crye-Leike Realtors</h4>
+                        <div class="subheading mb-3">Both Jamie and Tami are affiliate brokers working for Crye-Leike</div>
+                        {{--<div>Computer Science - Web Development Track</div>
+                        <p>GPA: 3.23</p>--}}
+                    </div>
+                    <div class="resume-date text-md-right">
+                        <p>Awarded: July 2019</p>
+                    </div>
+                </div>
+
+                <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="resume-content">
+                        <h4>Certification of Relocation Specialists</h4>
+                        <div class="subheading mb-3">Both Jamie and Tami are certified through LeadingRE</div>
+                        {{--<div>Computer Science - Web Development Track</div>
+                        <p>GPA: 3.23</p>--}}
+                    </div>
+                    <div class="resume-date text-md-right">
+                        <p>Awarded: July 2019</p>
+                    </div>
+                </div>
+
             </div>
-
-            <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
-                <div class="resume-content">
-                    <h4>National Realtor Certificate</h4>
-                    <div class="subheading mb-3">Certificate signifying nationwide acceptance of their accomplishments</div>
-                    {{--<div>Computer Science - Web Development Track</div>
-                    <p>GPA: 3.23</p>--}}
-                </div>
-                <div class="resume-date text-md-right">
-                    <span class="text-primary" style="color:black;">Awarded: July 2019</span>
-                </div>
-            </div>
-
-            <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
-                <div class="resume-content">
-                    <h4>National Realtor Certificate</h4>
-                    <div class="subheading mb-3">Certificate signifying nationwide acceptance of their accomplishments</div>
-                    {{--<div>Computer Science - Web Development Track</div>
-                    <p>GPA: 3.23</p>--}}
-                </div>
-                <div class="resume-date text-md-right">
-                    <span class="text-primary" style="color:black;">Awarded: July 2019</span>
-                </div>
-            </div>
-
-            <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
-                <div class="resume-content">
-                    <h4>Crye-Leike Realtors</h4>
-                    <div class="subheading mb-3">Both Jamie and Tami are affiliate brokers working for Crye-Leike</div>
-                    {{--<div>Computer Science - Web Development Track</div>
-                    <p>GPA: 3.23</p>--}}
-                </div>
-                <div class="resume-date text-md-right">
-                    <span class="text-primary" style="color:black;">Awarded: July 2019</span>
-                </div>
-            </div>
-
-            <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
-                <div class="resume-content">
-                    <h4>Certification of Relocation Specialists</h4>
-                    <div class="subheading mb-3">Both Jamie and Tami are certified through LeadingRE</div>
-                    {{--<div>Computer Science - Web Development Track</div>
-                    <p>GPA: 3.23</p>--}}
-                </div>
-                <div class="resume-date text-md-right">
-                    <span class="text-primary" style="color:black;">Awarded: July 2019</span>
-                </div>
-            </div>
-
-
-
         </div>
     </section>
 
